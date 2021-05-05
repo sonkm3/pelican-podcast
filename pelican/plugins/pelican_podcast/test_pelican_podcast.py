1d3317ae1b08a4e007f50c839deee2ba01ffde7c
sonkmr <sonkm3@gmail.com>
2021-05-05 20:21:15 +0900

    update cookiecutter template. (#3)
    * update workflow.
    
    * update cookiecutter template.
    
    * fix code format and remove ignored file.
    
    * black

--- pelican/plugins/pelican_podcast/test_pelican_podcast.py
+++ pelican/plugins/pelican_podcast/test_pelican_podcast.py
@@ -22,8 +22,8 @@ PODCAST_FEED_PATH = "feeds/podcasts.atom.xml"
 PODCAST_FEED_TITLE = "SITENAME"
 PODCAST_FEED_EXPLICIT = "No"  # todo:
 PODCAST_FEED_LANGUAGE = "ja"  # todo:
-PODCAST_FEED_COPYRIGHT = "COPYRIGHT STRING"  #todo:
-PODCAST_FEED_SUBTITLE = "SUBTITLE STRING"  #todo:
+PODCAST_FEED_COPYRIGHT = "COPYRIGHT STRING"  # todo:
+PODCAST_FEED_SUBTITLE = "SUBTITLE STRING"  # todo:
 PODCAST_FEED_AUTHOR = "AUTHOR STRING"  # todo:
 PODCAST_FEED_SUMMARY = "SUMMERY STRING"  # todo:
 PODCAST_FEED_IMAGE = SITEURL + "/images/common/artwork.jpg"  # todo:
@@ -121,24 +121,26 @@ class TestiTunesWriter(unittest.TestCase):
 
     def get_generator(self):
         category = Category("misc", self.settings)
-        podcast_articles = [get_article(
-            "podcast title",
-            "podcast cocntent",
-            podcast="http://example.com/audio/test.mp3",
-            category=category,
-            date=datetime.datetime.now(),
-            length="120",
-            duration="120",
-        ),
-        get_article(
-            "podcast title",
-            "podcast cocntent",
-            podcast="http://example.com/audio/test.mp3",
-            category=category,
-            date=datetime.datetime.now(),
-            length="120",
-            duration="120",
-        )]
+        podcast_articles = [
+            get_article(
+                "podcast title",
+                "podcast cocntent",
+                podcast="http://example.com/audio/test.mp3",
+                category=category,
+                date=datetime.datetime.now(),
+                length="120",
+                duration="120",
+            ),
+            get_article(
+                "podcast title",
+                "podcast cocntent",
+                podcast="http://example.com/audio/test.mp3",
+                category=category,
+                date=datetime.datetime.now(),
+                length="120",
+                duration="120",
+            ),
+        ]
 
         context = get_context(**self.settings)
         context["articles"] = podcast_articles
@@ -153,7 +155,6 @@ class TestiTunesWriter(unittest.TestCase):
         generator.generate_context()
         return generator
 
-
     def test_generate_output_article(self):
         generator = self.get_generator()
 
@@ -165,10 +166,25 @@ class TestiTunesWriter(unittest.TestCase):
 
         with open(output_path) as output_file:
             output_containts = output_file.read()
-            assert(output_containts.count("<title>podcast title</title>"))
-            assert(output_containts.count('type="audio/mpeg" url="http://example.com/audio/test.mp3"') == 2)
-            assert(output_containts.count("<link>http://example.com//podcast-title.html</link>") == 2)
-            assert(output_containts.count("<guid>http://example.com//podcast-title.html</guid>") == 2)
+            assert output_containts.count("<title>podcast title</title>")
+            assert (
+                output_containts.count(
+                    'type="audio/mpeg" url="http://example.com/audio/test.mp3"'
+                )
+                == 2
+            )
+            assert (
+                output_containts.count(
+                    "<link>http://example.com//podcast-title.html</link>"
+                )
+                == 2
+            )
+            assert (
+                output_containts.count(
+                    "<guid>http://example.com//podcast-title.html</guid>"
+                )
+                == 2
+            )
             self.assertIn('length="960975"', output_containts)
             self.assertIn("<itunes:duration>60</itunes:duration>", output_containts)
 
@@ -185,12 +201,17 @@ class TestiTunesWriter(unittest.TestCase):
             output_containts = output_file.read()
             self.assertIn('<?xml version="1.0" encoding="utf-8"?>', output_containts)
             self.assertIn(
-                '<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">',
+                '<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">',  # NOQA E501
                 output_containts,
             )
             self.assertIn("Leisure", output_containts)
-            self.assertIn("Hobbies", output_containts)            
-            self.assertIn('<itunes:name>OWNER STRING</itunes:name>', output_containts)
-            self.assertIn('<itunes:email>example@example.com</itunes:email>', output_containts)
-            self.assertIn('<itunes:owner><itunes:name>OWNER STRING</itunes:name><itunes:email>example@example.com</itunes:email></itunes:owner>', output_containts)
-            assert(output_containts.count("<title>A Pelican Blog</title>") == 1)
+            self.assertIn("Hobbies", output_containts)
+            self.assertIn("<itunes:name>OWNER STRING</itunes:name>", output_containts)
+            self.assertIn(
+                "<itunes:email>example@example.com</itunes:email>", output_containts
+            )
+            self.assertIn(
+                "<itunes:owner><itunes:name>OWNER STRING</itunes:name><itunes:email>example@example.com</itunes:email></itunes:owner>",  # NOQA E501
+                output_containts,
+            )
+            assert output_containts.count("<title>A Pelican Blog</title>") == 1
